a7a9bf327943eea753c65f2e98507be958785c89
enio23 <enio.gjerga@gmail.com>
2019-04-20 12:35:58 +0200

    Update genesymbol2uniprot.R
    Former-commit-id: 1a3f1c130cbb2c22a94fc22107de79a9efe582cc
    Former-commit-id: d8e85f67aea2fc67e990d7e5aacb7d665e999f87

--- R/genesymbol2uniprot.R
+++ R/genesymbol2uniprot.R
@@ -12,7 +12,7 @@
 #' @export
 
 GeneSymbol2Uniprot=function(df, map, geneID=1, uniprotID=2){
-  library(dplyr)
+  # library(dplyr)
   df$genesymbols_for_mapping<-rownames(df)
   colnames(map)[geneID]<-'genesymbols_for_mapping'
   colnames(map)[uniprotID]<-'uniprotids_for_mapping'
